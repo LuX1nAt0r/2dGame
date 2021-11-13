96fd91361ccfbcb426689bf916737990e3390f15
LuX1nAt0r PC <l.landsteiner@t-online.de>
2021-11-13 17:33:53 +0100

    Added KeyListener

--- src/main/java/engine/Window.java
+++ src/main/java/engine/Window.java
@@ -14,6 +14,10 @@ public class Window {
     private String title;
     private long glfwWindow;
 
+
+    private float r, g, b, a;
+    private boolean fadeToBlack = false;
+
     private static Window window = null;
 
 
@@ -21,6 +25,10 @@ public class Window {
         this.width = 1920;
         this.height = 1080;
         this.title = "Mario";
+        r = 1;
+        b = 1;
+        g = 1;
+        a = 1;
     }
 
     public static Window get() {
@@ -70,6 +78,8 @@ public class Window {
         glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
         glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
         glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
+
 
         //Make the OpenGL context current
         glfwMakeContextCurrent(glfwWindow);
@@ -89,15 +99,26 @@ public class Window {
         // bindings available for use.
         GL.createCapabilities();
 
-        //Set the clear color
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
 
         while (!glfwWindowShouldClose(glfwWindow)) {
             //Poll events
             glfwPollEvents();
 
-
+            //Set the clear color
+            glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT);
+
+            if (fadeToBlack) {
+                r = Math.max(r - 0.01f, 0);
+                g = Math.max(g - 0.01f, 0);
+                b = Math.max(b - 0.01f, 0);
+            }
+
+            if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)) {
+                fadeToBlack = true;
+
+            }
 
             glfwSwapBuffers(glfwWindow);
         }
