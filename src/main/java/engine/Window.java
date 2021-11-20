79fc78fa524ec8c65ab48ebcbcac27749d275743
LuX1nAt0r PC <l.landsteiner@t-online.de>
2021-11-20 20:42:46 +0100

    Drawing Square is working

--- src/main/java/engine/Window.java
+++ src/main/java/engine/Window.java
@@ -108,17 +108,20 @@ public class Window {
         //Make the window visible
         glfwShowWindow(glfwWindow);
 
-        Window.changeScene(0);
 
-    }
 
-    public void loop() {
         // This line is critical for LWJGL's interoperation with GLFW's
         // OpenGL context, or any context that is managed externally.
         // LWJGL detects the context that is current in the current thread,
         // creates the GLCapabilities instance and makes the OpenGL
         // bindings available for use.
         GL.createCapabilities();
+
+        Window.changeScene(0);
+    }
+
+    public void loop() {
+
 
         float beginTime = Time.getTime();
         float endTime;
